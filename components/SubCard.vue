cfc7152df9ea9e084166eefc31b983b919ac2607
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2022-01-04 16:35:13 +0100

    Primitive cards

---
+++ components/SubCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="subCard">
+    <!-- <nuxt-img :src="`/images/${content.img}`"></nuxt-img> -->
+    <nuxt-picture src="/images/kultur.jpg"></nuxt-picture>
+    <div class="box">
+      <h4>{{ content }}</h4>
+      <p v-if="cardType === 'contact'">
+        {{ contact.name }}<br />
+        {{ contact.number }}<br />
+        {{ contact.email }}
+      </p>
+      <p v-if="cardType === 'event'">
+        {{ event.date }}<br />
+        {{ event.loc }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardType: {
+      default() {},
+      type: Object,
+    },
+    content: {
+      default: '',
+      type: String,
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/det-mener-vi/_slug.vue
+++ pages/det-mener-vi/_slug.vue
@@ -2,23 +2,27 @@
   <div class="article nav-offset wrapper">
     <main>
       <h2>{{ article[0].title }}</h2>
-      <nuxt-content :document="article[0]" class="offset-top"></nuxt-content>
-      <ul>
-        <li v-for="program in article[0].programmer" :key="program">
-          <nuxt-link :to="`/det-mener-vi/princip-programmer/${program.toLowerCase().replaceAll(' ', '-')}`">{{
-            program
-          }}</nuxt-link>
-        </li>
-      </ul>
+      <nuxt-content :document="article[0]" class="offset-bottom"></nuxt-content>
+      <section class="card-grid">
+        <nuxt-link
+          v-for="program in article[0].programmer"
+          :key="program"
+          :to="`/det-mener-vi/princip-programmer/${program
+            .toLowerCase()
+            .replaceAll(' ', '-')}`"
+          ><sub-card :content="program"></sub-card
+        ></nuxt-link>
+      </section>
     </main>
     <SubNavDMV></SubNavDMV>
   </div>
 </template>
 
 <script>
+import SubCard from '~/components/SubCard.vue'
 import SubNavDMV from '~/components/SubNavDMV.vue'
 export default {
-  components: { SubNavDMV },
+  components: { SubNavDMV, SubCard },
   async asyncData({ $content, params }) {
     const article = await $content('det-mener-vi', params.slug).fetch()
 
