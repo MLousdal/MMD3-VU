8275cee24a2eeb499a04f48d4b16a274b4364519
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2022-01-16 15:09:44 +0100

    Setup

---
+++ components/VMap.vue
@@ -0,0 +1,11 @@
+<template>
+  <nuxt-link to="/lokalforeninger" class="map" aria-label="lokalforeninger">
+    <nuxt-picture src="/images/map.png" alt=""></nuxt-picture>
+  </nuxt-link>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
