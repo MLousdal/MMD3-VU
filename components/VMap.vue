c252f0b318a06dc1430c5b58b9dcb27b440f5d8c
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2022-01-17 13:29:17 +0100

    links

--- components/VMap.vue
+++ components/VMap.vue
@@ -1,10 +1,23 @@
 <template>
-  <svg-map :map="Danmark" />
+  <svg-map :map="Danmark" @click="clickLocation" />
 </template>
 
 <script>
 import { SvgMap } from 'vue-svg-map'
+import { getLocationName } from '../utilities'
 import Danmark from '~/assets/danmark'
+
+const URL = {
+  midtjylland: '/lokalforeninger/midt-vestjylland',
+  østjylland: '/lokalforeninger/østjylland',
+  nordjylland: '/lokalforeninger/nordjylland',
+  sydjylland: '/lokalforeninger/syd-sønderjylland',
+  fyn: '/lokalforeninger/fyn',
+  hovedstaden: '/lokalforeninger/hovedstaden',
+  nordsjælland: '/lokalforeninger/nordsjælland',
+  bornholm: '/lokalforeninger/hovedstaden',
+  sjælland: '/lokalforeninger/sjælland',
+}
 
 export default {
   name: 'MyMap',
@@ -15,6 +28,13 @@ export default {
     return {
       Danmark,
     }
+  },
+  methods: {
+    clickLocation(event) {
+      this.clickedLocation = getLocationName(event.target)
+      this.$router.push(URL[event.target.id])
+      // window.open(URL[event.target.id], '_blank')
+    },
   },
 }
 </script>
