440c0e82acfd97d1d8f86cea723c5d12eeca6874
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2022-01-17 15:26:38 +0100

    New lokal page

--- pages/lokalforeninger/_slug.vue
+++ pages/lokalforeninger/_slug.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="article nav-offset wrapper">
+    <v-map class="lokal"></v-map>
     <main class="">
       <h1>{{ article.title }}</h1>
       <nuxt-content :document="article" class="offset-bottom"></nuxt-content>
-      <v-map class="lokal"></v-map>
     </main>
-    <sub-nav title="Find Lokalforeninger" :links="links" dir></sub-nav>
   </div>
 </template>
 
@@ -15,15 +14,9 @@ export default {
   components: { VMap },
   async asyncData({ $content, params }) {
     const [article] = await $content('lokalforeninger').fetch()
-    const links = await $content('lokalforeninger', { deep: true })
-      .only(['title', 'dir'])
-      .where({ slug: 'index' })
-      .sortBy('title', 'asc')
-      .fetch()
 
     return {
       article,
-      links,
     }
   },
   head() {
